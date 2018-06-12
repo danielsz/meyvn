c15685147bd053f5df46a726ea7d1dd2d28caec1
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2018-06-13 02:10:17 +0300

    README

--- src/meyvn/utils.clj
+++ src/meyvn/utils.clj
@@ -1,5 +1,19 @@
-(ns meyvn.utils)
+(ns meyvn.utils
+  (:import [java.util Scanner]))
 
 (defn exit [msg & {:keys [status] :or {status 0}}]
   (println msg)
   (System/exit status))
+
+(defn prompt []
+  (let [sc (Scanner. (System/in))
+        resp (try (-> sc
+                      (.next (re-pattern "[Yy]|[Nn]")))
+                  (catch InputMismatchException e
+                    (println "Please answer [Y]es or [N]o")
+                    nil))]
+    (or resp (recur))))
+
+(defn opt-in []
+  
+  )
